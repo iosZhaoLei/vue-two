138e39477c902492b4d7a5a7df2873f8833bcdec
iOSZhaoLei <zlan521@qq.com>
2019-08-03 08:46:46 +0800

    zl

--- yanxuan-app/src/App.vue
+++ yanxuan-app/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<div id="app">
+  <keep-alive>
     <router-view/>
-  </div>
+  </keep-alive>
+
+  <tab-bar/>
+</div>
 </template>
 
 <style lang="scss">
